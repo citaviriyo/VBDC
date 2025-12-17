2ceeae6a9651e58027dfd0200f12800f300f774c
Citaviriyo <citaviriyo@gmail.com>
2025-12-17 23:01:00 +0700

    Fix iframe allowFullScreen type

--- src/app/kontak/page.tsx
+++ src/app/kontak/page.tsx
@@ -113,7 +113,7 @@ export default function Kontak() {
                   width="100%" 
                   height="100%" 
                   style={{border: '0'}} 
-                  allowFullScreen="" 
+                  allowFullScreen 
                   loading="lazy" 
                   referrerPolicy="no-referrer-when-downgrade">
                 </iframe>
