8a4c8221bd01dfcf9cd110b902292b97456e97ef
Citaviriyo <citaviriyo@gmail.com>
2026-03-28 18:34:30 +0700

    fix: update base URL in sitemap and change HTML language attribute to Indonesian

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,7 +45,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="id" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground`}
       >
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://www.vbdc.or.id";
+  const baseUrl = "https://vbdc.or.id";
 
   return [
     {
